d704f437359778b70494532bb8917405504d3b48
Matthias Gernand <matthias@gernand.tech>
2026-04-11 16:40:24 +0200

    Added logger mock extensions.

---
+++ src/Fluxera.Extensions.Logging.Mock/MockLoggerExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.Extensions.Logging.Mock
+{
+	using JetBrains.Annotations;
+	using Moq;
+
+    /// <summary>
+    ///		Extension methods for the <see cref="Mock{T}"/> type.
+    /// </summary>
+    [PublicAPI]
+	public static class MockLoggerExtensions
+	{
+		/// <summary>
+		///     The entry point for any logger mock verification.
+		/// </summary>
+		/// <typeparam name="T">The type of the generic parameter of the logger.</typeparam>
+		/// <param name="logger">The logger mock instance.</param>
+		/// <returns>The verify builder.</returns>
+		public static IVerifyBuilder<T> VerifyLog<T>(this Mock<ILogger<T>> logger)
+		{
+			return new VerifyBuilder<T>(logger);
+		}
+
+		/// <summary>
+		///     The entry point for any logger mock verification.
+		/// </summary>
+		/// <param name="logger">The logger mock instance.</param>
+		/// <returns>The verify builder.</returns>
+		public static IVerifyBuilder VerifyLog(this Mock<ILogger> logger)
+		{
+			return new VerifyBuilder(logger);
+		}
+	}
+}
